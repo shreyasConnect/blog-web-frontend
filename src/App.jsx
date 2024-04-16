8bd08818a7adb2b285eeaaeb3b4634b686b2700b
shreyasConnect <shreyas18.dev@gmail.com>
2024-04-16 11:53:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
+import ForgotPassword from './pages/ForgotPassword';
+import VerifyOTP from './pages/VerifyOTP';
+import ResetPassword from './pages/ResetPassword';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/verify-otp' element={<VerifyOTP />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/update-post/:postId' element={<UpdatePost />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter >
+  );
+}
